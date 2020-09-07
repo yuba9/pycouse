6cc860f97b06ff05676e199b8f13f2823bcbbe3d
Yuba <yuvalbe9@gmail.com>
2020-09-07 15:23:41 +0300

    fist version

--- script#1.py
+++ script#1.py
@@ -1,1 +1,10 @@
-print("hello")
+
+while (1):
+    x = int(input("enter a number: "))
+
+    if (x % 7) == 0:
+        print ("number divides by 7")
+    else:
+        print("number does not divide by 7")
+
+print(x)
