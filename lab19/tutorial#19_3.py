38f901286af6e15c62261f57f5f142ee2675ab95
Yuba <yuvalbe9@gmail.com>
2020-09-19 23:11:07 +0300

    first version of tutorial 19.3

---
+++ lab19/tutorial#19_3.py
@@ -0,0 +1,19 @@
+import sys
+import re
+
+file_handler = open('lab19\hosts.txt', "r")
+general_lines = file_handler.readlines()
+file_handler.close()
+
+hosts = {}
+
+for line in general_lines:
+    key = re.search(r'^(.*)\=(.*)$', line).group(1)
+    value = re.search(r'^(.*)\=(.*)$', line).group(2)
+    hosts[key] = value
+
+for i in sys.argv[1:]:
+    try:
+        print(hosts[i])
+    except KeyError:
+        print("invalid host name")
