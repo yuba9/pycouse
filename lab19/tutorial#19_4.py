2443748bebcd250aeff6ccd526f0815061f8cca9
Yuba <yuvalbe9@gmail.com>
2020-09-20 01:40:25 +0300

    styling fixes

--- lab19/tutorial#19_4.py
+++ lab19/tutorial#19_4.py
@@ -4,33 +4,34 @@ file_handler = open('words.txt', "r")
 general_lines = file_handler.readlines()
 file_handler.close()
 
+
 # the core algorithm is to build a-b-c histogram of each word's letters and then compare them
 def is_anagram(str1, str2):
-    abc_historgram1 = {}
-    abc_historgram2 = {}
+    abc_histogram1 = {}
+    abc_histogram2 = {}
 
     if len(str1) != len(str2):
         return False
 
     for letter in str1:
-        if letter in abc_historgram1:
-            abc_historgram1[letter] = abc_historgram1[letter]+1
+        if letter in abc_histogram1:
+            abc_histogram1[letter] = abc_histogram1[letter]+1
         else:
-            abc_historgram1[letter] = int(1)
+            abc_histogram1[letter] = int(1)
 
     for letter in str2:
-        if letter in abc_historgram2:
-            abc_historgram2[letter] = abc_historgram2[letter]+1
+        if letter in abc_histogram2:
+            abc_histogram2[letter] = abc_histogram2[letter]+1
         else:
-            abc_historgram2[letter] = int(1)
+            abc_histogram2[letter] = int(1)
 
     is_identical_flag = 1
     for char_num in range(97, 110):
         char = chr(char_num)
-        if char in abc_historgram1:
-            if char in abc_historgram2:
-                if abc_historgram1[char] != abc_historgram2[char]:
-                 is_identical_flag = 0
+        if char in abc_histogram1:
+            if char in abc_histogram2:
+                if abc_histogram1[char] != abc_histogram2[char]:
+                    is_identical_flag = 0
             else:
                 is_identical_flag = 0
 
@@ -38,8 +39,6 @@ def is_anagram(str1, str2):
         return True
     else:
         return False
-
-
 
 
 final_list = []
@@ -54,16 +53,16 @@ while True:
         if idx >= len(general_lines):
             temp_list.append(general_lines[0].rstrip())
             general_lines.pop(0)
-            break;
+            break
         if is_anagram(general_lines[0].rstrip(), general_lines[idx].rstrip()):
             temp_list.append(general_lines[idx].rstrip())
             general_lines.pop(idx)
             continue
         else:
-            idx = idx +1
-    final_list.append(temp_list)   #adding entry (not concatanating)
+            idx = idx + 1
+    final_list.append(temp_list)   # adding entry (not concatenating)
 
 for i in final_list:
     for j in i:
-        print(j," ",end ="")
+        print(j, " ", end="")
     print("")
