529d3d1471b0e9c325ce6531e0f9a573f1b4c89f
Uri <uri@speedy.net>
2020-09-23 08:47:44 +0300

    Uri's comments to exercise 5.

--- tutorial#5_1.py
+++ tutorial#5_1.py
@@ -1,3 +1,25 @@
 n = int(input("Please Enter your age (in years): "))
 y = n*12
 print ("your age (in months) is:", y)
+
+"""
+Uri's comments:
+==============
+
+* Very good! This code works.
+* I recommend styling your code according to PEP-8. You can also use PyCharm's
+  Code -> Reformat Code feature. For example, spaces around "*", spaces after comma etc.
+  This is relevant to all your exercises.
+* Here is how your code looks reformatted:
+
+n = int(input("Please Enter your age (in years): "))
+y = n * 12
+print("your age (in months) is:", y)
+
+* It's better to end a Python file with a line break.
+  If you use PyCharm's Code -> Reformat Code feature, it will be done automatically.
+  This is relevant to all your exercises.
+* It's better to create a folder for each exercise and put the files in it.
+* It's better not to use special characters such as "#" in file names.
+  
+"""
